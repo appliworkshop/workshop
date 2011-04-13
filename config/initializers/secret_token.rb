176774d91bff9ac02f19cf61f51c567dcd3a2c11
Mariusz <mariuszworkshop@gmail.com>
2011-04-13 22:37:43 +0200

    App scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workshop::Application.config.secret_token = '8594cd72ed0b79819be5e46c7722576e8e6269b09dd185a8f4f7d903130f88565b4687e4bae7878e046ef4e03e0381b1d0e4465be904e4c4e3e7077eea21d5f0'
